1d6d6cd2befea96eb22794229ec53f1af8e17bc0
enricoteterra <enricoteterra@penguin.lxd>
2021-01-29 12:46:22 +1300

    move static files to public directory so the public folder can simply be deployed

--- src/shop/features/has-product-info.spec.js
+++ src/shop/features/has-product-info.spec.js
@@ -1,7 +1,7 @@
 describe("product info feature", () => {
     describe("inform customer about product scenario", () => {
         it("should have relevant product information", () => {
-            cy.visit('index.html')
+            cy.visit('public/index.html')
             cy.get('[data-cy=product-info] [data-cy=title]')
             cy.get('[data-cy=product-info] [data-cy=description]')
             cy.get('[data-cy=product-info] [data-cy=image]')

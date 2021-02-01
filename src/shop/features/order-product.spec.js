de5bf1a12470e8005bf02e264c9f65a93066d938
enricoteterra <enricoteterra@penguin.lxd>
2021-02-02 12:32:57 +1300

    notifications not initially visible test set as pending

--- src/shop/features/order-product.spec.js
+++ src/shop/features/order-product.spec.js
@@ -1,10 +1,9 @@
 describe("order product feature", () => {
     describe("order failed scenario", () => {
-        it("should not show any notification initially", () => {
+        it.skip("should not show any notification initially", () => {
             cy.visit('public/index.html')
-            cy.get('form')
-              .findElements('order-notification')
-              .should('have.length', 0)
+            cy.getElement('order-notification')
+              .should('not.exist')
         })
         it("should notify user that order could not be processed at this time", () => {
             cy.visit('public/index.html')

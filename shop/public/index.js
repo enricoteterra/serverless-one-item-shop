971ab1b24db10130cafe67e3e7fd009753ea670f
enricoteterra <enricoteterra@penguin.lxd>
2021-02-03 11:41:03 +1300

    index html file again only concerned with view composition

--- shop/public/index.js
+++ shop/public/index.js
@@ -1,2 +1,5 @@
 import './components/order-button.js'
 import './components/order-notification.js'
+
+document.addEventListener("order-placed", 
+    () => document.querySelector('order-notification').visible = true);
